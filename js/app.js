771bb077beac226e382cfb213877131f1c6bbfe6
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-30 22:37:25 -0500

    Creando prototype para mostrar error o exito en el submit

--- js/app.js
+++ js/app.js
@@ -24,6 +24,28 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+// Muestra alerta en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div');
+
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // Insertar en el html
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+}
+
 // Instanciando UI
 const ui = new UI();
 // console.log(ui);
@@ -45,9 +67,19 @@ function cotizarSeguro(e) {
 
     // Leer el año seleccionado
     const year = document.querySelector('#year').value;
-
+    
     // Leer el tipo de covertura
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    
+    if(marca === '' || year === '') {
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
+    }
 
-    console.log('cotizando');
+    ui.mostrarMensaje('Cotizando...', 'exito');
+
+    // Instanciar el seguro
+
+    // Utilizar el prototype que va a cotizar
+
 }
